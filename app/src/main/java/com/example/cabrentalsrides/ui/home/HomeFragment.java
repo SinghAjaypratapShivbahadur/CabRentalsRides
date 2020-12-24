f7301eb6b4e4841b5fa9524c2ef446059b5438bf
Ajaypratap S Singh <ajaypsingh2630@gmail.com>
2020-12-24 20:07:50 +0530

    video12 done.

--- app/src/main/java/com/example/cabrentalsrides/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cabrentalsrides/ui/home/HomeFragment.java
@@ -73,7 +73,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
         return root;
     }
-//
+
 
     private void init() {
         locationRequest = new LocationRequest();
